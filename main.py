46afe8c0d414f7c69607930cc54371fae78f109e
Danless <danlessrs@gmail.com>
2023-10-31 05:54:09 -0400

    ready for deploy

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
 import pandas as pd
 from fastapi import FastAPI
-from recomendacion_juego import recomendacion_juego
-from developer_review import developer_endpoint
-from user_for_genre import UserForGenre
-from user_data import UserData
-from analyze_developer_data import analyze_developer_data
-from best_developer_year import BestDeveloperYear
-from fastapi.middleware.cors import CORSMiddleware
+from src.recomendacion_juego import recomendacion_juego
+from src.developer_review import developer_endpoint
+from src.user_for_genre import UserForGenre
+from src.user_data import UserData
+from src.analyze_developer_data import analyze_developer_data
+from src.best_developer_year import BestDeveloperYear
 
 app = FastAPI()
 
 
-origins = [
-    "http://localhost.onrender.com",
-    "https://localhost.onrender.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-async def main():
-    return {"message": "Bienvenido a mi proyecto."}
 
 
 #Endpoint 1
 
-@app.get("recomendacion_juego/{id_producto}")
-async def get_recomendacion_juego(id_producto: int):
+@app.get("/recomendacion_juego/{id_producto}")
+def get_recomendacion_juego(id_producto: int):
     '''Ingresa el ID de un Juego y te recomendara 5 similares. 
 
     Ejemplo: { recomendaciones': [{ 'id': 679460,'title': 'Morendar: Goblin Slayer'},{'id': 770380,'title': 'Army of Tentacles: (Not) A Cthulhu Dating Sim: Black GOAT of the Woods Edition'}]}
@@ -48,19 +28,19 @@ async def get_recomendacion_juego(id_producto: int):
 
 #Endpoint 2
 
-@app.get("developer_review/{nombre_desarolladora}")
-def developer(desarrolladora: str):
+@app.get("/developer_review/{nombre_desarolladora}")
+def developer(nombre_desarolladora: str):
     '''Se devuelve un diccionario con el nombre del desarrollador como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor positivo o negativo.
     
     Ejemplo de retorno: {'Valve' : [Negative = 182, Positive = 278]}
     '''
     
-    return developer_endpoint(desarrolladora)
+    return developer_endpoint(nombre_desarolladora)
 
 
 #Endpoint 3
 
-@app.get("user_for_genre/{genero}")
+@app.get("/user_for_genre/{genero}")
 def user_for_genre(genero: str):
     '''
     Devuelve el ID  del usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
@@ -76,7 +56,7 @@ def user_for_genre(genero: str):
 
 #Entpoint 4
 
-@app.get("user_data/{user_id}")
+@app.get("/user_data/{user_id}")
 def user_data(user_id:str):
 
     '''
@@ -88,19 +68,19 @@ def user_data(user_id:str):
     return resultado
 
 
-@app.get('developer_data/{nombre_desarrolladora}')
-def developer_data(desarrolladora: str):
+@app.get('/developer_data/{nombre_desarrolladora}')
+def developer_data(nombre_desarrolladora: str):
 
     '''
     Cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora.
     
     '''
 
-    resultado = analyze_developer_data( desarrolladora)
+    resultado = analyze_developer_data(nombre_desarrolladora)
     return resultado
     
 
-@app.get("best_developer_year/{year}")
+@app.get("/best_developer_year/{year}")
 def best_developer_year(year: int):
 
     '''
